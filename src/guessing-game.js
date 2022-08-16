50b162c15061712ef018f9d1c111b861c097999a
Arhilapa <arhilapa1@gmail.com>
2022-08-16 19:43:58 +0300

    feat: task complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+	constructor() {
+		this.index = 2
+		this.little = 0
+		this.higher = 0
+		this.middle = 0
+	}
+	setRange(min, max) {
+		this.little = min
+		this.higher = max
+		this.middle = Math.round((this.higher - this.middle) / this.index)
+	}
+	guess() {
+		return this.middle
+	}
+	lower() {
+		this.higher = this.middle
+		this.middle =
+			this.little + Math.round((this.higher - this.little) / this.index)
+	}
+	greater() {
+		this.little = this.middle
+		this.middle =
+			this.middle + Math.round((this.higher - this.middle) / this.index)
+	}
 }
 
-module.exports = GuessingGame;
+module.exports = GuessingGame
